0c65d2840af36e128333ef12b4b6819f5619d238
piyusssshh <piyush@whozzat.com>
2023-09-13 16:28:55 +0530

    meta :: title and description added

--- src/components/head.js
+++ src/components/head.js
@@ -4,8 +4,8 @@ const head = () => {
   return (
     <>
       <Head>
-        <title>Hello there</title>
-        <meta name="description" content="A product from Walkover" />
+        <title>Viasocket: Unleash the Power of Seamless API Development</title>
+        <meta name="description" content="Elevate your API development experience with Viasocket, the ultimate developer tool that seamlessly combines the precision of a code editor with the simplicity of Postman. Viasocket empowers developers to effortlessly create, test, and manage APIs, transforming complex workflows into smooth, efficient processes. Explore the future of API development with Viasocket today!" />
       </Head>
     </>
   );
