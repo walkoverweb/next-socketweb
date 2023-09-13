b4263d6aa1685b320afb3fb3bbcc8962ae200e21
piyusssshh <piyush@whozzat.com>
2023-09-13 19:01:16 +0530

    img :: favico added

--- src/components/head.js
+++ src/components/head.js
@@ -6,6 +6,7 @@ const head = () => {
       <Head>
         <title>Viasocket: Unleash the Power of Seamless API Development</title>
         <meta name="description" content="Elevate your API development experience with Viasocket, the ultimate developer tool that seamlessly combines the precision of a code editor with the simplicity of Postman. Viasocket empowers developers to effortlessly create, test, and manage APIs, transforming complex workflows into smooth, efficient processes. Explore the future of API development with Viasocket today!" />
+        <link rel="icon" type="image/x-icon" href="/img/favico.svg"></link>
       </Head>
     </>
   );
