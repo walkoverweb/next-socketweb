632484c968d579220b88a33922cc500dd27182f8
adayush17 <adayush17@gmail.com>
2023-12-15 14:20:54 +0530

    remove popup and Update login

--- src/pages/login.js
+++ src/pages/login.js
@@ -1,52 +1,47 @@
+import { useEffect } from "react";
+
 const Login = () => {
-  function MyComponent() {
-      const configuration = {
-        referenceId: process.env. NEXT_PUBLIC_REFERENCE_ID,
-        success: (data) => {
-          // get verified token in response
-          console.log('success response', data)
-        },
-        failure: (error) => {z
-          // handle error
-          console.log('failure reason', error)
+  useEffect(() => {
+    const configuration = {
+      referenceId: process.env.NEXT_PUBLIC_REFERENCE_ID,
+      success: (data) => {
+        console.log("success response", data);
+      },
+      failure: (error) => {
+        console.log("failure reason", error);
+      },
+    };
+
+    const script = document.createElement("script");
+    script.type = "text/javascript";
+    script.onload = () => {
+      const checkInitVerification = setInterval(() => {
+        if (typeof initVerification === "function") {
+          clearInterval(checkInitVerification);
+          initVerification(configuration);
         }
-      }
-      const script = document.createElement('script')
-      script.type = 'text/javascript'
-      script.onload = () => {
-        const checkInitVerification = setInterval(() => {
-          if (typeof initVerification === 'function') {
-            clearInterval(checkInitVerification)
-            // eslint-disable-next-line no-undef
-            initVerification(configuration)
-          }
-        }, 100)
-      }
-      script.src = 'https://proxy.msg91.com/assets/proxy-auth/proxy-auth.js?time=34093049'
-      document.body.appendChild(script)
-      return () => {
-        // Cleanup code if needed
-        document.body.removeChild(script)
-      }
-  }
-  
+      }, 100);
+    };
+    script.src = "https://proxy.msg91.com/assets/proxy-auth/proxy-auth.js";
+
+    document.body.appendChild(script);
+    return () => {
+      document.body.removeChild(script);
+    };
+  });
+
   return (
     <>
       <div className="login d-flex gap-sm-0 gap-4 flex-column flex-sm-row-reverse">
-       
         <div className="login__main  col-12 col-sm-6 col-md-7 col-lg-8 col-xl9 p-2 p-sm-4 p-xl-5 ">
           <div className="login__main__loginbtn pt-4 pt-sm-0">
-          <img src="/img/mainlogo-dark.svg" alt="Via-Socket-logo" className="mobile-main-logo mb-3 d-block d-sm-none" />
+            <img
+              src="/img/mainlogo-dark.svg"
+              alt="Via-Socket-logo"
+              className="mobile-main-logo mb-3 d-block d-sm-none"
+            />
             <h2 className="t-dark">Welcome Back!</h2>
-            <button 
-            className="google-auth-btn   px-3 py-2  d-flex align-items-center gap-2" 
-            onClick={()=>MyComponent()}
-            
-            >
-              <img src="/img/google-logo.svg" />
-              Login WIth Google
-            </button>
-   
+            <div id={process.env.NEXT_PUBLIC_REFERENCE_ID} />
           </div>
           <div className="login__main__newac mt-5">
             <h2 className="t-dark">OR</h2>
@@ -56,8 +51,14 @@ const Login = () => {
           </div>
         </div>
         <div className="login__details  col-xl-3 col-lg-4 col-md-5 col-sm-6 col-12 p-2 p-sm-4 p-xl-5">
-          <img src="/img/mainlogo.svg" alt="Via-Socket-logo" className="d-none d-sm-block" />
-          <h4 className="mt-4">Unlock the Power of Seamless Backend Management</h4>
+          <img
+            src="/img/mainlogo.svg"
+            alt="Via-Socket-logo"
+            className="d-none d-sm-block"
+          />
+          <h4 className="mt-4">
+            Unlock the Power of Seamless Backend Management
+          </h4>
           <ul className="feature-list mt-3">
             <li>Backend Management Made Simple</li>
             <li>Create Flows with Ease</li>
