d9601a22151b383d3980b3033ecfac369faccbed
Rashiik <rashikhandelwal269@gmail.com>
2023-12-16 12:08:11 +0530

    blogs-added

---
+++ src/pages/blog/index.js
@@ -0,0 +1,35 @@
+import { listPostContent, countPosts } from "../../components/lib/posts";
+import { listTags } from "../../components/lib/tags";
+import Layout from "../../components/blogs/layout";
+import PostList from "../../components/blogs/postList";
+import config from "../../components/lib/config";
+
+export default function Index({ posts, tags, pagination }) {
+
+  const url = "/blog";
+  const title = "All posts";
+  return (
+    <Layout>
+      <PostList posts={posts} tags={tags} pagination={pagination} />
+    </Layout>
+  );
+}
+
+export async function getStaticProps() {
+  const posts = listPostContent(1, config.posts_per_page);
+  const tags = listTags();  
+  // const tagsObject = getTag();
+  // console.log(tagsObject, "inside tags object")
+  const pagination = {
+    current: 1,
+    pages: Math.ceil(countPosts() / config.posts_per_page),
+  };
+
+  return {
+    props: {
+      posts,
+        tags,
+      pagination,
+    },
+  };
+}
