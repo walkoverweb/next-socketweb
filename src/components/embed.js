0ce08b26e70505c601977fa4575c32c7f5e972bb
Saurabh Sharma <sawrabhsharma1234@gmail.com>
2023-12-19 19:45:33 +0530

    Desidning header and content

--- src/components/embed.js
+++ src/components/embed.js
@@ -1,31 +1,30 @@
 const Embed = () => {
   return (
     <>
-      <div className="embed  d-flex ">
-        <div className="embed__hero container h-100 d-flex w-100">
-          <div className="embed__hero__cont  col-8 d-flex flex-column gap-5 justify-content-center">
-            <div className="embed-hero-cont__heading">
-              <h1>The most advance</h1>
-              <h1>
-                <span className="tagnile-automation px-2 br-1">automation</span>{" "}
+      <div className="embed">
+        <div className="embed__hero container h-100 w-100">
+          <div className="embed__hero__cont  col-12">
+            <div className="embed-hero-cont__heading ">
+              <h1>The most advance
+                {" "} <span className="tagnile-automation px-2 rounded-2">automation</span>{" "}
                 tool ever built
               </h1>
             </div>
-            <h3 className="embed-hero-cont__text">
+            {/* <h3 className="embed-hero-cont__text">
               Connect 100+ apps or webhook to collect data, drag-and-drop
               functionality or write custom code using AI
-            </h3>
-            <div className="embed-hero-cont__btn d-flex align-items-center gap-3">
+            </h3> */}
+            <div className="embed-hero-cont__btn d-flex align-items-center gap-3 py-3">
               <button className="btn btn-light">Try for FREE</button>
               <a className="text-white">Compare with other tools</a>
             </div>
           </div>
-          <div className="embed__hero__img ">
-            <img className="hero-img" src="./img/embed/herol-img.svg"/>
+          <div className="embed__hero__img w-100">
+            <img className="hero-img  " width="100%" src="/img/main.svg" />
           </div>
         </div>
       </div>
-      
+
     </>
   );
 };
