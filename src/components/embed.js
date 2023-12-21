6548e0c46ae67fa09abb4bfc894bb5f7ad772e2f
Saurabh Sharma <sawrabhsharma1234@gmail.com>
2023-12-21 12:41:33 +0530

    Spacing

--- src/components/embed.js
+++ src/components/embed.js
@@ -2,7 +2,7 @@ const Embed = () => {
   return (
     <>
       <div className="embed">
-        <div className="embed__hero container h-100 w-100">
+        <div className="embed__hero container h-100 w-100 pt-5">
           <div className="embed__hero__cont  col-12">
             <div className="embed-hero-cont__heading my-2 ">
               <h1>The most advance <span className="d-inline-block position-relative tagnile-automation px-2 rounded-2">automation</span> tool ever built
@@ -14,11 +14,11 @@ const Embed = () => {
             </h3> */}
             <div className="embed-hero-cont__btn d-flex align-items-center gap-3 my-3">
               <button className="btn btn-light">Try for FREE</button>
-              <a className="text-white">Compare with other tools</a>
+              <a className="btn btn-outline-light">Compare with other tools</a>
             </div>
           </div>
-          <div className="embed__hero__img mt-4">
-            <img className="img-fluid" width="100%" src="/img/main.svg" />
+          <div className="embed__hero__img my-5">
+            <img className="img-fluid rounded-1" width="100%" src="/img/main.svg" />
           </div>
         </div>
       </div>
--- src/components/navbar.js
+++ src/components/navbar.js
@@ -4,36 +4,36 @@ import { MdMenu } from "react-icons/md";
 const Navbar = () => {
   return (
     <>
-      <nav class="navbar navbar-cont navbar-expand-lg">
-        <div class=" nav container d-flex  align-items-center  my-3">
+      <nav className="navbar navbar-cont navbar-expand-lg">
+        <div className=" nav container d-flex  align-items-center  my-3">
           <div>
             <a href="#">
               <img className="nav__logo navbar-brand" src="/img/mainlogo.svg" alt="Via-Socket-logo" />
             </a>
           </div>
-          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-            {/* <span class="navbar-toggler-icon menu"></span> */}
+          <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            {/* <span className="navbar-toggler-icon menu"></span> */}
             <MdMenu className="fs-1 text-white" />
           </button>
 
-          <div class="navbar-collapse nav__menu" id="navbarSupportedContent">
+          <div className="navbar-collapse nav__menu" id="navbarSupportedContent">
 
-            <ul class="navbar-nav ms-auto mb-2">
-              <li class="nav-item pe-3">
-                <a class="nav-link active tabitems-nav" href="#">Embed</a>
+            <ul className="navbar-nav ms-auto  d-flex align-items-center">
+              <li className="nav-item pe-3">
+                <a className="nav-link active tabitems-nav" href="#">Embed</a>
               </li>
-              <li class="nav-item pe-3 ">
-                <a class="nav-link tabitems-nav" href="#">Flow</a>
+              <li className="nav-item pe-3">
+                <a className="nav-link tabitems-nav" href="#">Flow</a>
               </li>
-              <li class="nav-item pe-3">
-                <a class="nav-link tabitems-nav" href="#">Table</a>
+              <li className="nav-item pe-3">
+                <a className="nav-link tabitems-nav" href="#">Table</a>
               </li>
             </ul>
 
 
             <div className="nav__buttons d-flex gap-3 ms-auto">
               <button type="button" className="btn btn-light">Try for FREE</button>
-              <a href="/login" className="btn btn-outline-light text-white">
+              <a href="/login" className="btn btn-outline-light">
                 login
               </a>
             </div>
